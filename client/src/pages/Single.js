6b7feb119afc104f08fb44cab321d9e63ad0a45a
broman1234 <manmo431@gmail.com>
2024-06-09 16:04:04 +0800

    set up single page content and its styl

--- client/src/pages/Single.js
+++ client/src/pages/Single.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import Delete from "../img/delete.png"
+import Edit from "../img/edit.png"
+import {Link} from "react-router-dom";
 
 const Single = () => {
     return (
-        <div>
-            Single
+        <div className="single">
+            <div className="content">
+                <img
+                    src="https://images.pexels.com/photos/1018447/pexels-photo-1018447.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    alt=""/>
+                <div className="user">
+                    <img
+                        src="https://images.pexels.com/photos/1018447/pexels-photo-1018447.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                        alt=""/>
+                    <div className="info">
+                        <span>Man</span>
+                        <p>Posted 2 days ago</p>
+                    </div>
+                    <div className="edit">
+                        <Link to={`/write?edit=2`}>
+                            <img src={Edit} alt=""/>
+                        </Link>
+                        <img src={Delete} alt=""/>
+                    </div>
+                </div>
+                <h1>Post 1 - This is a longer title for the post</h1>
+                <p>This is the content of post 1. This content is longer than the previous content and provides more
+                    details about the post.This is the content of post 1. This content is longer than the previous content and provides more
+                    details about the post. This is the content of post 1. This content is longer than the previous content and provides more
+                    details about the post.This is the content of post 1. This content is longer than the previous content and provides more
+                    details about the post. This is the content of post 1. This content is longer than the previous content and provides more
+                    details about the post.This is the content of post 1. This content is longer than the previous content and provides more
+                    details about the post.</p>
+            </div>
+            <div className="menu">m</div>
         </div>
     );
 };
